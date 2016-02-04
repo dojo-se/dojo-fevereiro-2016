c1b25dcfd9aa440e382cd41c7d48db1077a2a263
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2016-02-04 20:40:21 -0300

    Teste besta 3

--- src/cmd/encontre-o-telefone/encontre-o-telefone.go
+++ src/cmd/encontre-o-telefone/encontre-o-telefone.go
@@ -6,9 +6,11 @@ func getTelefone(strTelefone string) (numTelefone string) {
     dict["A"] = "2"
     dict["B"] = "2"
     dict["C"] = "2"
+    dict["D"] = "3"
+    dict["-"] = "-"
     
     for index := 0; index < len(strTelefone); index++ {
-        numTelefone += "2"
+        numTelefone += dict[strTelefone[index:index+1]]
     }
     
     return
--- src/cmd/encontre-o-telefone/encontre-o-telefone_test.go
+++ src/cmd/encontre-o-telefone/encontre-o-telefone_test.go
@@ -17,3 +17,27 @@ func TestSimples_AC(t *testing.T) {
     t.Error("Número de telefone deveria ser '22', mas foi ", numTelefone)
   }
 }
+
+func TestSimples_ABC(t *testing.T) {
+  numTelefone := getTelefone("ABC")
+
+  if numTelefone != "222" {
+    t.Error("Número de telefone deveria ser '222', mas foi ", numTelefone)
+  }
+}
+
+func TestSimples_ABCD(t *testing.T) {
+  numTelefone := getTelefone("ABCD")
+
+  if numTelefone != "2223" {
+    t.Error("Número de telefone deveria ser '2223', mas foi ", numTelefone)
+  }
+}
+
+func TestSimples_ABCD_Hifen(t *testing.T) {
+  numTelefone := getTelefone("ABCD-")
+
+  if numTelefone != "2223-" {
+    t.Error("Número de telefone deveria ser '2223-', mas foi ", numTelefone)
+  }
+}
