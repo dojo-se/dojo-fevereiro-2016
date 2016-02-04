17a9b8961fb1950f3f314ba540438b28a8e5776d
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2016-02-04 20:28:32 -0300

    Teste besta 2

--- src/cmd/encontre-o-telefone/encontre-o-telefone.go
+++ src/cmd/encontre-o-telefone/encontre-o-telefone.go
@@ -1,7 +1,15 @@
 package main
 
 func getTelefone(strTelefone string) (numTelefone string) {
-    numTelefone = "2"
+    numTelefone = ""
+    dict := make (map[string] string)
+    dict["A"] = "2"
+    dict["B"] = "2"
+    dict["C"] = "2"
+    
+    for index := 0; index < len(strTelefone); index++ {
+        numTelefone += "2"
+    }
     
     return
 }
--- src/cmd/encontre-o-telefone/encontre-o-telefone_test.go
+++ src/cmd/encontre-o-telefone/encontre-o-telefone_test.go
@@ -9,3 +9,11 @@ func TestSimples_A(t *testing.T) {
     t.Error("Número de telefone deveria ser '2', mas foi ", numTelefone)
   }
 }
+
+func TestSimples_AC(t *testing.T) {
+  numTelefone := getTelefone("AC")
+
+  if numTelefone != "22" {
+    t.Error("Número de telefone deveria ser '22', mas foi ", numTelefone)
+  }
+}
