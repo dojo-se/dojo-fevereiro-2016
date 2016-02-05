db6d53c82329b8c7be2cb66f6afe50fc404f6af0
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2016-02-04 21:18:03 -0300

    Testes Refatorados

--- src/cmd/encontre-o-telefone/encontre-o-telefone_test.go
+++ src/cmd/encontre-o-telefone/encontre-o-telefone_test.go
@@ -2,47 +2,26 @@ package main
 
 import "testing"
 
-func TestSimples_A(t *testing.T) {
-  numTelefone, _ := getTelefone("A")
-
-  if numTelefone != "2" {
-    t.Error("Número de telefone deveria ser '2', mas foi ", numTelefone)
-  }
+var testValues = map[string]string {
+    "A": "2",
+    "AC": "22",
+    "ABC": "222",
+    "ABCD": "2223",
+    "ABCD-": "2223-",
+    "The-quick-brown-fox-jumps-over-the-lazy-dog": "843-78425-27696-369-58677-6837-843-5299-364",
 }
 
-func TestSimples_AC(t *testing.T) {
-  numTelefone, _ := getTelefone("AC")
-
-  if numTelefone != "22" {
-    t.Error("Número de telefone deveria ser '22', mas foi ", numTelefone)
-  }
+func TestTestesDosValores(t *testing.T) {
+    for key, value := range(testValues) {
+        numTelefone, _ := getTelefone(key)
+        
+        if numTelefone != value {
+            t.Error("Número de telefone deveria ser ", value, ", mas foi ", numTelefone)
+        }
+    }
 }
 
-func TestSimples_ABC(t *testing.T) {
-  numTelefone, _ := getTelefone("ABC")
-
-  if numTelefone != "222" {
-    t.Error("Número de telefone deveria ser '222', mas foi ", numTelefone)
-  }
-}
-
-func TestSimples_ABCD(t *testing.T) {
-  numTelefone, _ := getTelefone("ABCD")
-
-  if numTelefone != "2223" {
-    t.Error("Número de telefone deveria ser '2223', mas foi ", numTelefone)
-  }
-}
-
-func TestSimples_ABCD_Hifen(t *testing.T) {
-  numTelefone, _ := getTelefone("ABCD-")
-
-  if numTelefone != "2223-" {
-    t.Error("Número de telefone deveria ser '2223-', mas foi ", numTelefone)
-  }
-}
-
-func TestSimples_ABCD_Espaço(t *testing.T) {
+func TestComNumeroInvalido(t *testing.T) {
   _, err := getTelefone("ABCD ")
 
   if err == "" {
@@ -50,12 +29,3 @@ func TestSimples_ABCD_Espaço(t *testing.T) {
   } 
 
 }
-
-func TestSimples_Frase(t *testing.T) {
-  numTelefone, _ := getTelefone("The-quick-brown-fox-jumps-over-the-lazy-dog")
-
-  if numTelefone != "843-78425-27696-369-58677-6837-843-5299-364" {
-    t.Error("Número de telefone deveria ser '843-78425-27696-369-58677-6837-843-5299-364', mas foi ", numTelefone)
-  }
-}
-
